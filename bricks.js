0fcf8ac713d8950001e0545629d920240921215d
AuWiMo <yodacanfly@gmail.com>
2022-01-11 16:18:21 -0500

    Update bricks.js

--- bricks.js
+++ bricks.js
@@ -19,11 +19,11 @@ function bricksGen(rows) {
 }
 
 function showWall() {
-  if(firstTry) {
-    fill("blue")
-  } else {
-    fill("green")
-  }
+  //if(firstTry) {
+  //  fill("blue")
+  //} else {
+  //  fill("green")
+  //}
   for (let i = 0; i < wall.length; i++) {
     rect(wall[i].x, wall[i].y, brickLength, brickThickness)
   }
