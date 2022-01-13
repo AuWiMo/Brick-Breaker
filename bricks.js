f6ee30894d293ded950a88f85e5d907f31826e4b
AuWiMo <yodacanfly@gmail.com>
2022-01-13 11:29:04 -0500

    Update bricks.js

--- bricks.js
+++ bricks.js
@@ -4,15 +4,36 @@ let brickThickness = 12;
 let wall = [];
 
 
-function Brick(xpos, ypos) {
+function Brick(xpos, ypos, color) {
   this.x = xpos;
   this.y = ypos;
+  this.color = color
 } 
 
 function bricksGen(rows) {
+  let iColor = 'red'
   for (let i = 0; i < rows; i++) {
+      if (i == 1) {
+        iColor = "orangered"
+      } else if (i == 2) {
+        iColor = "orange"
+      } else if (i == 3) {
+        iColor = "yellow"
+      } else if (i == 4) {
+        iColor = "lime"
+      } else if (i == 5) {
+        iColor = "limegreen"
+      } else if (i == 6) {
+        iColor = "mediumturquoise" 
+      } else if (i == 7) {
+        iColor = "blue"
+      } else if (i == 8) {
+        iColor = "darkviolet"
+      } else if (i == 9) {
+        iColor = "fuchsia"
+      }
       for(let j = 0; j < 10; j++) {
-        let b = new Brick(45 + j * 79, 75 + brickThickness/2 + i * (brickThickness * 50/45))
+        let b = new Brick(45 + j * 79, 75 + brickThickness/2 + i * (brickThickness * 50/45), iColor)
         wall.push(b);
       }
    }
@@ -20,6 +41,7 @@ function bricksGen(rows) {
 
 function showWall() {
   for (let i = 0; i < wall.length; i++) {
+    fill(wall[i].color)
     rect(wall[i].x, wall[i].y, brickLength, brickThickness)
   }
 }
